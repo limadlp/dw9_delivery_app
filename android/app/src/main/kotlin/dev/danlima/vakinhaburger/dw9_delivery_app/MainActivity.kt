b10498798c763cb94929adc13c808557ef7fa825
limadlp <dlplima@hotmail.com>
2023-02-02 17:11:08 -0300

    Aula 1 P1

---
+++ android/app/src/main/kotlin/dev/danlima/vakinhaburger/dw9_delivery_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.danlima.vakinhaburger.dw9_delivery_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/dw9_delivery_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Dw9DeliveryApp extends StatelessWidget {
+  const Dw9DeliveryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Delivery App',
+    );
+  }
+}
