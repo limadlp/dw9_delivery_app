de9d6ff3f652ce65a15ddac1cdd0741f4227cf15
limadlp <dlplima@hotmail.com>
2023-02-09 15:22:17 -0300

    Dia 3 - Parte 1

--- lib/app/dw9_delivery_app.dart
+++ lib/app/dw9_delivery_app.dart
@@ -2,6 +2,7 @@ import 'package:dw9_delivery_app/app/core/extensions/mouse_extension.dart';
 import 'package:dw9_delivery_app/app/core/provider/application_binding.dart';
 import 'package:dw9_delivery_app/app/core/ui/theme/theme_config.dart';
 import 'package:dw9_delivery_app/app/pages/home/home_router.dart';
+import 'package:dw9_delivery_app/app/pages/product_detail/product_detail_router.dart';
 import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -18,6 +19,7 @@ class Dw9DeliveryApp extends StatelessWidget {
         routes: {
           '/': (context) => const SplashPage(),
           '/home': (context) => HomeRouter.page,
+          '/productDetail': (context) => ProductDetailRouter.page,
         },
       ),
     );
